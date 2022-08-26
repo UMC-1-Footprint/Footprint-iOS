7756da060e46b0c1710ae5fcb5bd24a37dd0585e
SojinLee <513sojin@naver.com>
2022-08-26 15:46:59 +0900

    [Set] UIColor extension 파일 추가

---
+++ FootprintIOS/FootprintIOS/Sources/Extension/UIColor+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+Ext.swift
+//  Footprint-iOS
+//
+//  Created by Sojin Lee on 2022/08/26.
+//  Copyright © 2022 Footprint-iOS. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class var blueM: UIColor? { return UIColor(named: "BlueM") }
+}
