d7e9e491580189eaacfae922397ede2e10c9786a
김영인 <0inn1220@gmail.com>
2022-08-29 17:08:39 +0900

    [Feat] #7 - 정보 입력 뷰컨 이동 연결

--- FootprintIOS/FootprintIOS/Sources/ViewController/Calendar/CalendarViewController.swift
+++ FootprintIOS/FootprintIOS/Sources/ViewController/Calendar/CalendarViewController.swift
@@ -8,10 +8,47 @@
 
 import UIKit
 
+import SnapKit
+
 class CalendarViewController: NavigationBarViewController {
+    
+    private lazy var infoButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("정보 입력 뷰컨 이동", for: .normal)
+        btn.setTitleColor(.black, for: .normal)
+        btn.titleLabel?.font = .systemFont(ofSize: 15, weight: .semibold)
+        return btn
+    }()
+    
     override func setupNavigationBar() {
         super.setupNavigationBar()
         
         setNavigationBarTitleText("캘린더")
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        view.addSubview(infoButton)
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        infoButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    override func setupBind() {
+        super.setupBind()
+        
+        infoButton.rx.tap
+            .bind { [weak self] in
+                let infoVC = InfoViewController(reactor: .init())
+                infoVC.hidesBottomBarWhenPushed = true
+                self?.navigationController?.pushViewController(infoVC, animated: true)
+            }
+            .disposed(by: disposeBag)
     }
 }
