d135aa9c8e31e3fb4cd79ac3a5219e4408477b3b
송영모 <mooyoung2309@gmail.com>
2022-08-31 16:45:27 +0900

    feat: 설정 페이지 UI 개발

--- FootprintIOS/FootprintIOS/Sources/Extension/UIColor+Ext.swift
+++ FootprintIOS/FootprintIOS/Sources/Extension/UIColor+Ext.swift
@@ -10,4 +10,15 @@ import UIKit
 
 extension UIColor {
     static var blueM: UIColor? { return UIColor(named: "BlueM") }
+    
+    // MARK: hex code를 이용하여 정의
+    // ex. UIColor(hex: 0xF5663F)
+    convenience init(hex: UInt, alpha: CGFloat = 1.0) {
+        self.init(
+            red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((hex & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(hex & 0x0000FF) / 255.0,
+            alpha: CGFloat(alpha)
+        )
+    }
 }
