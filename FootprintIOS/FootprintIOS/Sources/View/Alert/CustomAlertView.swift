116a4184c114d7a02fdd7b77a4aa34635c307c47
김영인 <0inn1220@gmail.com>
2022-09-06 18:35:27 +0900

    [Feat] #17 - 4가지 alertView 구현

---
+++ FootprintIOS/FootprintIOS/Sources/View/Alert/CustomAlertView.swift
@@ -0,0 +1,117 @@
+//
+//  CustomAlertView.swift
+//  Footprint-iOS
+//
+//  Created by 김영인 on 2022/09/06.
+//  Copyright © 2022 Footprint-iOS. All rights reserved.
+//
+
+import UIKit
+
+class CustomAlertView: BaseView {
+    
+    // MARK: - Properties
+    
+    let type: AlertType
+
+    // MARK: - UI Components
+    
+    private let backgroundView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 20
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 14, weight: .semibold)
+        $0.textColor = FootprintIOSAsset.Colors.blackM.color
+    }
+    
+    // FIXME: 나중에 재사용 가능성이 있다면 customView도 enum으로 관리
+    private let customView = UIView().then {
+        $0.backgroundColor = .systemYellow
+    }
+    
+    private let lineView = UIView().then {
+        $0.backgroundColor = FootprintIOSAsset.Colors.white3.color
+    }
+    
+    private lazy var cancelButton = UIButton().then {
+        $0.setTitle("취소", for: .normal)
+        $0.setTitleColor(FootprintIOSAsset.Colors.blackM.color, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
+    }
+    
+    private lazy var rightButton = UIButton().then {
+        $0.setTitleColor(FootprintIOSAsset.Colors.blueM.color, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
+    }
+    
+    private lazy var buttonStackView = UIStackView().then {
+        $0.addArrangedSubview(cancelButton)
+        $0.addArrangedSubview(rightButton)
+        $0.distribution = .fillEqually
+        $0.axis = .horizontal
+    }
+    
+    // MARK: - Initializer
+    
+    init(type: AlertType) {
+        self.type = type
+        
+        super.init(frame: .zero)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    
+    override func setupProperty() {
+        super.setupProperty()
+        
+        self.isHidden = true
+        titleLabel.text = type.title
+        rightButton.setTitle(type.buttonTitle, for: .normal)
+    }
+
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        addSubview(backgroundView)
+        backgroundView.addSubviews([titleLabel, customView, lineView, buttonStackView])
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        backgroundView.snp.makeConstraints {
+            $0.width.equalTo(326)
+            $0.height.equalTo(259)
+            $0.center.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(backgroundView.snp.top).offset(23)
+            $0.centerX.equalTo(backgroundView)
+        }
+        
+        customView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalTo(backgroundView)
+            $0.height.equalTo(150)
+        }
+        
+        buttonStackView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalTo(backgroundView)
+            $0.height.equalTo(56)
+        }
+        
+        lineView.snp.makeConstraints {
+            $0.bottom.equalTo(buttonStackView.snp.top)
+            $0.leading.trailing.equalTo(backgroundView)
+            $0.height.equalTo(1)
+        }
+    }
+}
---
+++ FootprintIOS/FootprintIOS/Sources/View/Alert/OneButtonAlertView.swift
@@ -0,0 +1,93 @@
+//
+//  OneButtonAlertView.swift
+//  Footprint-iOS
+//
+//  Created by 김영인 on 2022/09/06.
+//  Copyright © 2022 Footprint-iOS. All rights reserved.
+//
+
+import UIKit
+
+class OneButtonAlertView: BaseView {
+    
+    // MARK: - Properties
+    
+    let type: AlertType
+    
+    // MARK: - UI Components
+    
+    private let backgroundView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 20
+    }
+    
+    private let titleLabel = UILabel().then {
+        $0.font = .systemFont(ofSize: 16, weight: .semibold)
+        $0.textColor = FootprintIOSAsset.Colors.blackD.color
+    }
+    
+    private lazy var confirmButton = UIButton().then {
+        $0.setTitle("확인", for: .normal)
+        $0.setTitleColor(FootprintIOSAsset.Colors.blueM.color, for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .semibold)
+    }
+    
+    private let lineView = UIView().then {
+        $0.backgroundColor = FootprintIOSAsset.Colors.white3.color
+    }
+    
+    // MARK: - Initializer
+    
+    init(type: AlertType) {
+        self.type = type
+        
+        super.init(frame: .zero)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    
+    override func setupProperty() {
+        super.setupProperty()
+        
+        self.isHidden = true
+        titleLabel.text = type.title
+    }
+
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        addSubview(backgroundView)
+        backgroundView.addSubviews([titleLabel, lineView, confirmButton])
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        backgroundView.snp.makeConstraints {
+            $0.width.equalTo(340)
+            $0.height.equalTo(140)
+            $0.center.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(backgroundView).offset(37)
+            $0.centerX.equalToSuperview()
+        }
+        
+        lineView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(30)
+            $0.leading.trailing.equalTo(backgroundView)
+            $0.height.equalTo(1)
+        }
+        
+        confirmButton.snp.makeConstraints {
+            $0.top.equalTo(lineView.snp.bottom).offset(10)
+            $0.centerX.equalTo(backgroundView)
+        }
+    }
+}
