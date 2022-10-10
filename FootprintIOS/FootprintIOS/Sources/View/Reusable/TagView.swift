e240b1e93f086bf0ec251be761c91f9082144fb4
송영모 <mooyoung2309@gmail.com>
2022-10-10 23:56:04 +0900

    [Feat] #30 - 상단 뷰 생성

---
+++ FootprintIOS/FootprintIOS/Sources/View/Reusable/TagView.swift
@@ -0,0 +1,56 @@
+//
+//  TagView.swift
+//  Footprint-iOS
+//
+//  Created by 송영모 on 2022/10/10.
+//  Copyright © 2022 Footprint-iOS. All rights reserved.
+//
+
+import UIKit
+
+class TagView: BaseView {
+    
+    // MARK: - UI Components
+    
+    let label: UILabel = .init()
+    
+    // MARK: - Initializer
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        
+        label.text = title
+    }
+    
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup Methods
+    
+    override func setupProperty() {
+        super.setupProperty()
+        
+        label.textColor = FootprintIOSAsset.Colors.blackL.color
+        label.font = .systemFont(ofSize: 10)
+        
+        self.cornerRound(radius: 5)
+        self.backgroundColor = FootprintIOSAsset.Colors.whiteM.color
+    }
+    
+    override func setupHierarchy() {
+        super.setupHierarchy()
+        
+        addSubviews([label])
+    }
+    
+    override func setupLayout() {
+        super.setupLayout()
+        
+        label.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(1)
+            $0.leading.trailing.equalToSuperview().inset(8)
+        }
+    }
+}
--- FootprintIOS/FootprintIOS/Sources/ViewController/Record/RecordViewController.swift
+++ FootprintIOS/FootprintIOS/Sources/ViewController/Record/RecordViewController.swift
@@ -10,16 +10,72 @@ import UIKit
 import NMapsMap
 
 class RecordViewController: NavigationBarViewController {
+    
+    // MARK: - UI Components
+    
     let mapView: NMFMapView = .init()
+    let topView: UIView = .init()
+    let timeLabel: UILabel = .init()
+    let distanceTagView: TagView = .init(title: "거리")
+    let distanceLabel: UILabel = .init()
+    let calorieTagView: TagView = .init(title: "칼로리")
+    let calorieLabel: UILabel = .init()
+    let paceTagView: TagView = .init(title: "페이스")
+    let paceLabel: UILabel = .init()
+    let toggleButton: UIButton = .init()
+    let divider1: UIView = .init()
+    let divider2: UIView = .init()
+    let progressView: UIView = .init()
+    let progressBarView: UIView = .init()
+    
+    // MARK: - Setup Methods
+    
+    override func setupNavigationBar() {
+        super.setupNavigationBar()
+        
+        setNavigationBarTitleText("실시간 기록")
+    }
     
     override func setupProperty() {
         super.setupProperty()
+        
+        topView.backgroundColor = .white
+        
+        timeLabel.text = "17:21"
+        timeLabel.font = .systemFont(ofSize: 28, weight: .bold)
+        timeLabel.textColor = .black
+        
+        distanceLabel.text = "21 km"
+        distanceLabel.textColor = FootprintIOSAsset.Colors.blackD.color
+        distanceLabel.font = .systemFont(ofSize: 14)
+        
+        calorieLabel.text = "120 kcal"
+        calorieLabel.textColor = FootprintIOSAsset.Colors.blackD.color
+        calorieLabel.font = .systemFont(ofSize: 14)
+        
+        paceLabel.text = "100 걸음/분"
+        calorieLabel.textColor = FootprintIOSAsset.Colors.blackD.color
+        paceLabel.font = .systemFont(ofSize: 14)
+        
+        divider1.backgroundColor = FootprintIOSAsset.Colors.whiteD.color
+        
+        divider2.backgroundColor = FootprintIOSAsset.Colors.whiteD.color
+        
+        toggleButton.setImage(.init(systemName: "chevron.down"), for: .normal)
+        toggleButton.tintColor = .black
+        
+        progressView.backgroundColor = FootprintIOSAsset.Colors.whiteD.color
+        
+        progressBarView.backgroundColor = FootprintIOSAsset.Colors.blueM.color
+        progressBarView.cornerRound(radius: 4, direct: [.layerMaxXMinYCorner, .layerMaxXMaxYCorner])
     }
     
     override func setupHierarchy() {
         super.setupHierarchy()
         
-        contentView.addSubviews([mapView])
+        contentView.addSubviews([mapView, topView])
+        topView.addSubviews([timeLabel, distanceTagView, distanceLabel, divider1, calorieTagView, calorieLabel, divider2, paceTagView, paceLabel, toggleButton, progressView])
+        progressView.addSubviews([progressBarView])
     }
     
     override func setupLayout() {
@@ -27,6 +83,75 @@ class RecordViewController: NavigationBarViewController {
         
         mapView.snp.makeConstraints {
             $0.top.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        topView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(80)
+        }
+        
+        timeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(14)
+            $0.leading.equalToSuperview().inset(16)
+        }
+        
+        distanceTagView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(14)
+            $0.leading.equalTo(timeLabel.snp.trailing).offset(30)
+        }
+        
+        distanceLabel.snp.makeConstraints {
+            $0.top.equalTo(distanceTagView.snp.bottom).offset(4)
+            $0.leading.equalTo(distanceTagView)
+        }
+        
+        divider1.snp.makeConstraints {
+            $0.leading.equalTo(distanceTagView.snp.trailing).offset(20)
+            $0.centerY.equalTo(timeLabel)
+            $0.width.equalTo(1)
+            $0.height.equalTo(16)
+        }
+        
+        calorieTagView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(14)
+            $0.leading.equalTo(divider1.snp.trailing).offset(10)
+        }
+        
+        calorieLabel.snp.makeConstraints {
+            $0.top.equalTo(calorieTagView.snp.bottom).offset(4)
+            $0.leading.equalTo(calorieTagView)
+        }
+        
+        divider2.snp.makeConstraints {
+            $0.leading.equalTo(calorieTagView.snp.trailing).offset(20)
+            $0.centerY.equalTo(timeLabel)
+            $0.width.equalTo(1)
+            $0.height.equalTo(16)
+        }
+        
+        paceTagView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(14)
+            $0.leading.equalTo(divider2.snp.trailing).offset(10)
+        }
+        
+        paceLabel.snp.makeConstraints {
+            $0.top.equalTo(paceTagView.snp.bottom).offset(4)
+            $0.leading.equalTo(paceTagView)
+        }
+        
+        toggleButton.snp.makeConstraints {
+            $0.centerY.equalTo(timeLabel)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        
+        progressView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(8)
+        }
+        
+        progressBarView.snp.makeConstraints {
+            $0.top.leading.bottom.equalToSuperview()
+            $0.trailing.equalToSuperview().multipliedBy(0.3)
         }
     }
 }
