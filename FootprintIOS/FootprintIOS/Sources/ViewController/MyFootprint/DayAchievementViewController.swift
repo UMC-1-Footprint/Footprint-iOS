484cebd42125660fb37166cb8f734b4941bafa09
SojinLee <513sojin@naver.com>
2022-11-18 22:46:10 +0900

    [Feat] #31 - 그래프 뷰 추가

--- FootprintIOS/FootprintIOS/Sources/ViewController/MyFootprint/DayAchievementViewController.swift
+++ FootprintIOS/FootprintIOS/Sources/ViewController/MyFootprint/DayAchievementViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class DayAchievementViewController: BaseViewController {
     let percentageView = AttainmentPercentageView(endPoint: 100, increasementPoint: 20)
     let lineView = PercentageLineView()
+    let backgroundView = UIView()
+    let percentages = [60,70,80,80,90,50,60]
+    lazy var barGraphStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.distribution = .equalSpacing
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupGraphView(percentages)
     }
     
     override func setupLayout() {
@@ -30,9 +37,44 @@ class DayAchievementViewController: BaseViewController {
             $0.leading.equalTo(percentageView.snp.trailing).offset(8)
             $0.top.equalTo(percentageView.snp.top).offset(5)
         }
+        
+        backgroundView.snp.makeConstraints {
+            $0.width.equalTo(320)
+            $0.height.equalTo(140)
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(15)
+        }
+        
+        barGraphStackView.snp.makeConstraints {
+            $0.width.equalTo(244)
+            $0.height.equalTo(125)
+            $0.bottom.equalTo(lineView.snp.bottom)
+            $0.centerX.equalTo(lineView)
+        }
     }
     
     override func setupHierarchy() {
-        view.addSubviews([percentageView, lineView])
+        backgroundView.addSubviews([percentageView, lineView])
+        view.addSubviews([backgroundView, barGraphStackView])
+    }
+    
+    func setupGraphView(_ percentages: [Int]) {
+        for i in 0...6 {
+            let graph = BarGraphView(percentage: percentages[i])
+            
+            let gradientLayer = CAGradientLayer()
+            gradientLayer.frame = view.bounds
+            
+            let colors: [CGColor] = [
+                FootprintIOSAsset.Colors.blueM.color.cgColor,
+               UIColor.white.cgColor
+            ]
+            gradientLayer.colors = colors
+            gradientLayer.locations = [0.01, 0.15]
+
+            graph.mainGraphView.layer.addSublayer(gradientLayer)
+            
+            barGraphStackView.addArrangedSubview(graph)
+        }
     }
 }
