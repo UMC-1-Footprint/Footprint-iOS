fc71f7936cbf440136e328f0abf1a6104a52dd57
김영인 <0inn1220@gmail.com>
2022-11-19 17:48:19 +0900

    [Add] #24 - 재사용 뷰 추가
    textField & selectView

---
+++ FootprintIOS/FootprintIOS/Sources/View/UserInfo/UserInfoLabel.swift
@@ -0,0 +1,32 @@
+//
+//  UserInfoLabel.swift
+//  Footprint-iOS
+//
+//  Created by 김영인 on 2022/11/19.
+//  Copyright © 2022 Footprint-iOS. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoLabel: UILabel {
+    
+    // MARK: - Properties
+    
+    let star: String = "*"
+    
+    // MARK: - Initiailizer
+    
+    init(title: String) {
+        super.init(frame: .zero)
+        
+        text = title
+        textColor = FootprintIOSAsset.Colors.blackD.color
+        font = .systemFont(ofSize: 14, weight: .semibold)
+        color(string: star, color: FootprintIOSAsset.Colors.blueM.color)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
