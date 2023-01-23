4dc7822f865073f5873ee81d776135c5f07b460c
김영인 <0inn1220@gmail.com>
2023-01-23 19:24:51 +0900

    [Feat] #62 - EndPoint 및 Service 구축

--- FootprintIOS/FootprintIOS/Sources/Network/EndPoint/InfoEndPoint.swift
+++ FootprintIOS/FootprintIOS/Sources/Network/EndPoint/InfoEndPoint.swift
@@ -9,30 +9,47 @@
 import Foundation
 
 enum InfoEndPoint {
-    case requestUserInfo(userInfo: InfoRequestDTO)
+    case postUserInfo(userInfo: InfoRequestDTO)
+    case getThisMonth
+    case getNextMonth
+    case patchNextMonthGoal(goalInfo: GoalRequestDTO)
 }
 
 extension InfoEndPoint: EndPoint {
     var url: String {
         switch self {
-        case .requestUserInfo:
+        case .postUserInfo:
             return Provider.shared.Enviroment.url + "/users/infos"
+        case .getThisMonth, .patchNextMonthGoal:
+            return Provider.shared.Enviroment.url + "/users/goals"
+        case .getNextMonth:
+            return Provider.shared.Enviroment.url + "/users/goals/next"
         }
     }
     
     var method: HTTPMethod {
         switch self {
-        case .requestUserInfo:
+        case .postUserInfo:
             return .POST
+        case .getNextMonth, .getThisMonth:
+            return .GET
+        case .patchNextMonthGoal:
+            return .PATCH
         }
     }
     
     var body: Data? {
         switch self {
-        case let .requestUserInfo(userInfo):
+        case let .postUserInfo(userInfo):
             guard let body = try? JSONEncoder().encode(userInfo) else { return nil }
             
             return body
+        case let .patchNextMonthGoal(goalInfo):
+            guard let body = try? JSONEncoder().encode(goalInfo) else { return nil }
+            
+            return body
+        default:
+            return nil
         }
     }
 }
--- FootprintIOS/FootprintIOS/Sources/Service/InfoService.swift
+++ FootprintIOS/FootprintIOS/Sources/Service/InfoService.swift
@@ -12,41 +12,88 @@ enum InfoEvent {
     case updateBirth(content: String)
     case updateWalk(content: String)
     case updateGoalWalk(content: String)
+    
+    case getThisMonthGoal(GoalResponseDTO)
+    case getNextMonthGoal(GoalResponseDTO)
+    case patchNextMonthGoal(Bool)
 }
 
 protocol InfoServiceProtocol {
     var event: PublishSubject<InfoEvent> { get }
     
-    func requestInfo(userInfo: UserInfoRequestModel, goalInfo: GoalInfoDTO)
+    func postUserInfo(userInfo: UserInfoRequestModel, goalInfo: GoalRequestDTO)
+    func getThisMonthGoal()
+    func getNextMonthGoal()
+    func patchNextMonthGoal(goalInfo: GoalRequestDTO)
     func updateBirth(to birth: String) -> Observable<String>
     func updateWalk(to walk: String) -> Observable<String>
     func updateGoalWalk(to goalWalk: String) -> Observable<String>
 }
 
 class InfoService: NetworkService, InfoServiceProtocol {
-    
     let event = PublishSubject<InfoEvent>()
     
-    func requestInfo(userInfo: UserInfoRequestModel, goalInfo: GoalInfoDTO) {
+    func postUserInfo(userInfo: UserInfoRequestModel, goalInfo: GoalRequestDTO) {
         let request = InfoEndPoint
-            .requestUserInfo(
+            .postUserInfo(
                 userInfo: InfoRequestDTO(
                     nickname: userInfo.nickname,
                     sex: userInfo.sex,
                     birth: userInfo.birth,
-                    height: Int(userInfo.height ?? "0") ?? 0,
-                    weight: Int(userInfo.weight ?? "0") ?? 0,
+                    height: Int(userInfo.height ?? "0") ?? nil,
+                    weight: Int(userInfo.weight ?? "0") ?? nil,
                     dayIdx: goalInfo.dayIdx,
                     walkGoalTime: goalInfo.walkGoalTime,
                     walkTimeSlot: goalInfo.walkTimeSlot))
             .createRequest()
         
-        let response: Single<DefaultResponse> = API.request(request: request)
+        API.request(request: request)
+            .asObservable()
+            .map(\DefaultResponse.message)
+            .bind {
+                print($0)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func getThisMonthGoal() {
+        let request = InfoEndPoint
+            .getThisMonth
+            .createRequest()
         
-        response.asObservable()
-            .map(\.message)
-            .bind { data in
-                print("\(data)")
+        API.request(request: request)
+            .asObservable()
+            .map(\BaseModel.result)
+            .bind { [weak self] data in
+                self?.event.onNext(.getThisMonthGoal(data))
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func getNextMonthGoal() {
+        let request = InfoEndPoint
+            .getNextMonth
+            .createRequest()
+        
+        API.request(request: request)
+            .asObservable()
+            .map(\BaseModel.result)
+            .bind { [weak self] data in
+                self?.event.onNext(.getNextMonthGoal(data))
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    func patchNextMonthGoal(goalInfo: GoalRequestDTO) {
+        let request = InfoEndPoint
+            .patchNextMonthGoal(goalInfo: goalInfo)
+            .createRequest()
+        
+        API.request(request: request)
+            .asObservable()
+            .map(\DefaultResponse.isSuccess)
+            .bind { [weak self] isSuccess in
+                self?.event.onNext(.patchNextMonthGoal(isSuccess))
             }
             .disposed(by: disposeBag)
     }
